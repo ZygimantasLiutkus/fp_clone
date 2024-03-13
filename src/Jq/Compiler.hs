d617b69e444ad38f0085be4f96bcf1d7f2fc5fe2
zygis <liutkus@student.tudelft.nl>
2024-03-13 13:25:27 +0100

    Filters and compiler for filters

--- src/Jq/Compiler.hs
+++ src/Jq/Compiler.hs
@@ -8,6 +8,56 @@ type JProgram a = JSON -> Either String a
 
 compile :: Filter -> JProgram [JSON]
 compile (Identity) inp = return [inp]
+compile (Parenthesis f) inp = compile f inp
+compile (ObjIndex s) inp = case inp of
+  JObject o -> case lookup s o of
+    Just v -> return [v]
+    Nothing -> return [JNull]
+  JNull -> return [JNull]
+  _ -> Left "Object index not applicable"
+compile (OptObjIndex s) inp = case inp of
+  JObject o -> case lookup s o of
+    Just v -> return [v]
+    Nothing -> return [JNull]
+  JNull -> return [JNull]
+  _ -> return []
+compile (ArrIndex i) inp = case inp of
+  JArray a -> if i < length a && i >= 0
+                then return [a !! i]
+                else if i < 0 && abs i <= length a
+                  then return [a !! (length a + i)]
+                  else return [JNull]
+  JNull -> return [JNull]
+  _ -> Left "Array index not applicable"
+compile (Slice from to) inp = case inp of
+  JArray a -> case (from, to) of
+    (f, t) | f > 0 && t > 0 -> return $ take (t - f) $ drop f a
+    (f, t) | f < 0 && t > 0 -> let f' = if abs f > length a then 0 else length a + f
+                               in return $ take (t - f') $ drop f' a
+    (f, t) | f > 0 && t < 0 -> let t' = if abs t > length a then 0 else length a + t
+                               in return $ take (t' - f) $ drop f a
+    (f, t) | f < 0 && t < 0 -> let f' = if abs f > length a then 0 else length a + f
+                                   t' = if abs t > length a then 0 else length a + t
+                               in return $ take (t' - f') $ drop f' a
+    _ -> return []
+  JNull -> return [JNull]
+  _ -> Left "Slice not applicable"
+compile (Iterator) inp = case inp of
+  JArray a -> return a
+  JObject o -> return $ map snd o
+  _ -> Left "Iterator not applicable"
+compile (OptIterator) inp = case inp of
+  JArray a -> return a
+  JObject o -> return $ map snd o
+  _ -> return []
+compile (Comma f1 f2) inp = do
+  r1 <- compile f1 inp
+  r2 <- compile f2 inp
+  return $ r1 ++ r2
+compile (Pipe f1 f2) inp = do
+  r1 <- compile f1 inp
+  r2 <- mapM (compile f2) r1
+  return $ concat r2
 
 run :: JProgram [JSON] -> JSON -> Either String [JSON]
 run p j = p j
--- src/Jq/Filters.hs
+++ src/Jq/Filters.hs
@@ -1,13 +1,32 @@
 module Jq.Filters where
 
-data Filter = Identity
+data Filter = Identity | Parenthesis Filter | ObjIndex String | OptObjIndex String |
+              ArrIndex Int | Slice Int Int | Iterator | OptIterator | Comma Filter Filter | Pipe Filter Filter
 
 instance Show Filter where
   show (Identity) = "."
+  show (Parenthesis f) = "(" ++ show f ++ ")"
+  show (ObjIndex s) = "." ++ s
+  show (OptObjIndex s) = "." ++ s ++ "?"
+  show (ArrIndex i) = ".["++show i++"]"
+  show (Slice from to) = ".["++ show from ++":"++show to++"]"
+  show (Iterator) = ".[]"
+  show (OptIterator) = ".[]?"
+  show (Comma f1 f2) = show f1 ++ " , " ++ show f2
+  show (Pipe f1 f2) = show f1 ++ " | " ++ show f2
 
 instance Eq Filter where
   Identity == Identity = True
-  _ == _ = undefined
+  Parenthesis f1 == Parenthesis f2 = f1 == f2
+  ObjIndex s1 == ObjIndex s2 = s1 == s2
+  OptObjIndex s1 == OptObjIndex s2 = s1 == s2
+  ArrIndex i == ArrIndex j = i == j
+  Slice i j == Slice x y = i == x && j == y
+  Iterator == Iterator = True
+  OptIterator == OptIterator = True
+  Comma f1 f2 == Comma f3 f4 = f1 == f3 && f2 == f4
+  Pipe f1 f2 == Pipe f3 f4 = f1 == f3 && f2 == f4
+  _ == _ = False
 
 data Config = ConfigC {filters :: Filter}
 
@@ -21,10 +40,10 @@ filterIdentitySC :: Filter
 filterIdentitySC = Identity
 
 filterStringIndexingSC :: String -> Filter
-filterStringIndexingSC = undefined
+filterStringIndexingSC s = ObjIndex s
 
 filterPipeSC :: Filter -> Filter -> Filter
-filterPipeSC = undefined
+filterPipeSC f1 f2 = Pipe f1 f2
 
 filterCommaSC :: Filter -> Filter -> Filter
-filterCommaSC = undefined
+filterCommaSC f1 f2 = Comma f1 f2
